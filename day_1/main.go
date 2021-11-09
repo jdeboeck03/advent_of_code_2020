8d0fc5e6e61d0b4754707aa24a42611659d87f74
Jelle De Boeck <jelle.deboeck@axxes.com>
2021-11-09 16:50:53 +0100

    Finished Day 1 challenge

---
+++ day_1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// loads input of file into a slice of integer variables
+func loadInput(filename string) ([]int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var input []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_int, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+		input = append(input, line_int)
+	}
+	return input, nil
+}
+
+func main() {
+	filename := "input"
+	input, err := loadInput(filename)
+	if err != nil {
+		log.Fatalf("Program failed: %s", err)
+	}
+	// Double (Now Triple) for loop that sums every input integer with all integers in input slice except its own
+	// Clean Code
+	for i, first := range input {
+		for j, second := range input {
+			for k, third := range input {
+				if i != j && i != k && j != k {
+					sum := first + second + third
+					if sum == 2020 {
+						fmt.Printf("First: %d Second: %d Third: %d Sum: %d\n", first, second, third, sum)
+						fmt.Printf("VICTORY\n")
+						product := first * second * third
+						fmt.Printf("Product: %d\n", product)
+					}
+				}
+			}
+		}
+	}
+	sum := input[0] + input[1]
+	fmt.Println(sum)
+}
